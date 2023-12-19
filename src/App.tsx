0d2316db0aa1ed12ece7231270c08f949c527fdb
RichGuilherme <richard.guilhermeAS@hotmail.com>
2023-12-18 21:55:21 -0300

    feat: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import { MusicList } from "./components/MusicList"
-import { PlayComponent } from "./components/PlayComponent "
-import { SideBar } from "./components/SideBar"
-import { ViewApp } from "./styles/global"
+import { Home } from "./components/Home"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Authentication } from "./components/Authentication";
+import { ReactNode } from "react";
 
+type Props = {
+  children: ReactNode
+}
 
 function App() {
+  const PrivateRoute = ( {children}: Props ) => {
+    const isAuthenticated = true
+
+    return isAuthenticated ? (
+      children
+    ) : (
+      <Navigate to="/authentication" replace />
+    )
+  }
 
   return (
     <div className="app">
-      <ViewApp>
-        <SideBar />
-        <MusicList />
-        <PlayComponent />
-      </ViewApp>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/authentication" element={<Authentication />} />
+          <Route path="/home" element={<PrivateRoute> <Home /></PrivateRoute> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
