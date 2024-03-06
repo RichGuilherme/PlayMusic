57e318c67526ca37962c7c1d7c464e9207b567b4
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-03-06 13:10:48 -0300

    feat: outlet na rota home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,13 +1,19 @@
-import { MusicList } from './MusicList'
+
+import { Outlet } from 'react-router-dom'
 import { PlayComponent } from './PlayComponent '
 import { SideBar } from './SideBar'
 import { ViewApp } from './style'
 
+
 export const Home = () => {
+    // const [searchParams, setSearchParams] = useSearchParams()
+
+    // console.log(searchParams)
+
     return (
         <ViewApp>
             <SideBar />
-            <MusicList />
+            <Outlet />
             <PlayComponent />
         </ViewApp>
     )
